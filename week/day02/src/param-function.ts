f7151926778d7c5da497146db1807fbe66a3098b
24wings <731502029@qq.com>
2016-11-10 09:32:40 +0800

    submit

--- week/day02/src/param-function.ts
+++ week/day02/src/param-function.ts
@@ -2,7 +2,10 @@ var sayHello = function () {
     console.log('hello');
 }
 
-// () invoke 调用
+//  () invoke 调用 
+/**
+ * @param {Function} func 是一个函数参数
+ */
 var execute = function (func) {
     func();
 }
